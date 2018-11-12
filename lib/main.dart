ad006d594034ab5d753824f0fbaef22d71b92cb7
Mayank Khanna <khanna98@outlook.com>
2018-11-12 15:59:55 +0530

    Changes in File

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CupertinoPageScaffold(
-        navigationBar: new CupertinoNavigationBar(
-          leading: Icon(CupertinoIcons.back),
-          middle: Text("CODING101"),
-          trailing: Text("Exit"),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
+
+  TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(vsync: this,length: 4,initialIndex: 2);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.blue,
+          labelColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.camera_alt)),
+            new Tab(text: "CHATS",),
+            new Tab(text: "STATUS",),
+            new Tab(text: "CALLS",),
+          ],
         ),
-        child: Container(
-          color: CupertinoColors.white,
+        actions: <Widget>[
+          new Icon(Icons.search),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Icon(Icons.more_vert),
+          )
+        ],
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new CameraScreen(widget.cameras),
+          new ChatScreen(),
+          new StatusScreen(),
+          new CallsScreen(),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: new Icon(
+          Icons.message,
+          color: Colors.white,
         ),
+        onPressed: ()=>print("Open Chats"),
       ),
     );
+    
   }
 }
