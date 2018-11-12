d6a0bfc78dcd2d47b0c5e1ec4400335310ccf26d
Mayank Khanna <khanna98@outlook.com>
2018-11-12 23:18:33 +0530

    Chat Model

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -12,24 +12,24 @@ List<ChatModel> dummyData = [
     name: "Mayank Khanna",
     message: "Hey! how are you ?",
     time: "15:30",
-    avatarUrl:  "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb",
+    avatarUrl:"https://images.pexels.com/photos/842567/pexels-photo-842567.jpeg?cs=srgb&dl=adult-beard-blur-842567.jpg&fm=jpg",
   ),
   new ChatModel(
     name: "Sumant Khanna",
     message: "Hey! Flutter is amazing !",
     time: "14:30",
-    avatarUrl:  "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb",
+    avatarUrl:  "https://images.pexels.com/photos/842567/pexels-photo-842567.jpeg?cs=srgb&dl=adult-beard-blur-842567.jpg&fm=jpg",
   ),
   new ChatModel(
     name: "Harry",
     message: "Hey! Are you busy ?",
     time: "13:30",
-    avatarUrl:  "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb",
+    avatarUrl:  "https://images.pexels.com/photos/842567/pexels-photo-842567.jpeg?cs=srgb&dl=adult-beard-blur-842567.jpg&fm=jpg",
   ),
   new ChatModel(
     name: "Patrick",
     message: "Hey!",
     time: "12:30",
-    avatarUrl:  "http://www.usanetwork.com/sites/usanetwork/files/styles/629x720/public/suits_cast_harvey.jpg?itok=fpTOeeBb",
+    avatarUrl:  "https://images.pexels.com/photos/842567/pexels-photo-842567.jpeg?cs=srgb&dl=adult-beard-blur-842567.jpg&fm=jpg",
   ),
 ];
